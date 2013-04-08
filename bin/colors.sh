be6771d081bf9a95817f0abe181b3f4b78204034
Chris Hawkins <rc.hawkins@gmail.com>
2013-04-07 23:54:46 -0700

    minor

--- bin/colors.sh
+++ bin/colors.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-# Show all the colours of the rainbow, should be run under bash
+
 for STYLE in 0 1 4 7; do
   for FG in 30 31 32 33 34 35 36 37; do
     for BG in 40 41 42 43 44 45 46 47; do
